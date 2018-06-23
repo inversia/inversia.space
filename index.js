e17699bb55773cb2838556ce923e589858dca100
inversia <mrs.shpritz@gmail.com>
2018-06-23 08:18:33 +0300

    smooth scroll

--- index.js
+++ index.js
@@ -15,3 +15,38 @@ function scrollTo(hash) {
         block: 'start'
     })
 }
+
+
+document.addEventListener ('DOMContentLoaded', () => {          // код выполняющийся после загрузки DOM-дерева (содержимого document.body)
+
+    for (const link of document.querySelectorAll ('.link')) {   // для каждого элемента с селектором .link
+
+        link.addEventListener ('click', x => {
+
+            const href = link.getAttribute('href')              // <a href="/#services"> → /#services
+            const hash = href.replace('/', '')                  // /#services → #services
+
+            if (document.querySelector(hash)) {                 // есть ли на странице элемент отзывающийся на селектор #services
+                history.pushState(null, null, hash)             // заменяем в адресной строке адрес на /#services (но так, чтобы страница не прыгала к этому элементу)
+                scrollTo(hash)                                  // плавно прокручиваем страницу к #services
+                x.preventDefault()                              // предотвращаем дефолтное поведение клика на ссылку (чтобы страница не прыгала к #services)
+            }
+        })
+    }
+})
+
+// выделяет объекты <iframe> и отменяет им цветовой фильтр.
+
+let currentActiveElement
+
+setInterval (function () {
+    if (document.activeElement !== currentActiveElement) {
+
+        currentActiveElement = document.activeElement
+
+        if (currentActiveElement.tagName === 'IFRAME') {
+
+            currentActiveElement.style.filter = 'none';
+        }
+    }
+}, 100)
