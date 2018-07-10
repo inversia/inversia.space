e10260eaeb91807bb641ad93b6270fdeb921586c
inversia <mrs.shpritz@gmail.com>
2018-07-10 14:56:53 +0300

    new music tracks

--- audio.js
+++ audio.js
@@ -49,7 +49,9 @@ document.addEventListener ('DOMContentLoaded', () => {
 
     const tracks = [
         { name: 'Final cut', url: 'https://inversia.space/music/Final_cut.mp3' },
+        { name: 'Zimnee fano', url: 'https://inversia.space/music/Zimnee_fano.mp3' },
         { name: 'Stroll', url: 'https://inversia.space/music/Stroll.mp3' },
+        { name: 'Can you see me?', url: 'https://inversia.space/music/Can_you_see_me.mp3' },
         { name: 'Izvrasheniye svista', url: 'https://inversia.space/music/izvrasheniye_svista.mp3' },
         { name: 'Underwater tour', url: 'https://inversia.space/music/gentlewoman-Underwater_tour.mp3' },
         
