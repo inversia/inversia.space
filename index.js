afac2771bc8486ec8d76a84079cd4f1cb15719a8
inversia <mrs.shpritz@gmail.com>
2019-01-11 06:51:16 +0300

    video fix process

--- index.js
+++ index.js
@@ -58,7 +58,6 @@ document.addEventListener ('DOMContentLoaded', () => {
                 v.controls = true
             } else {
                 v.pause ()
-                v.controls = false
             }
         }
     }
